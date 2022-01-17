eafc9a483c461d4937a17feff58ee374145ff5bd
Ethereal-bang <939847757@qq.com>
2022-01-17 10:15:12 +0800

    update:自动选择天气图标

--- src/script/main.js
+++ src/script/main.js
@@ -28,6 +28,18 @@ import {
 
 const width = window.innerWidth;
 
+function judgeWeatherIcon(weather, time = 'day') {
+    if (weather === "晴") {
+        return (time === "day") ? 'sun.png' : 'moon.png';
+    } else if (weather === "多云") {
+        return (time === "day") ? "cloudy_day.png" : "cloudy_night";
+    } else if (weather === "阴") {
+        return "yin.png";
+    } else if (weather === "小雨") {
+        return "rain_small.png";
+    }
+}
+
 // 跳转到搜索页:
 {
     navigate.addEventListener("click", () => {
@@ -201,7 +213,7 @@ export function setWeekWeather() {
     })
         .then(data => {
             const {daily} = data;
-            // console.log(daily)
+            console.log(daily)
             /**
              * @desc 根据等级返回对应class
              * */
@@ -246,7 +258,7 @@ export function setWeekWeather() {
             daily.map(item => {
                 weatherArr.push({
                     day_weather: item.textDay,
-                    dayIcon: item.iconDay,
+                    dayIconUrl: judgeWeatherIcon(item.textDay),
                     nightIcons: item.iconNight,
                     night_weather: item.textNight,
                     wind: item.windDirDay,
@@ -265,12 +277,7 @@ export function setWeekWeather() {
                 // dayIcon_innerHTML += `<img
                 //     src="../../assets/qWeather_icons/101.svg"
                 //     alt="icon"/>`
-                dayIconsInner += `<li>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="qi-101" viewBox="0 0 16 16">
-                        <path d="M4.995 1.777a.516.516 0 0 0 .503.404.535.535 0 0 0 .112-.012.517.517 0 0 0 .392-.616L5.746.403A.516.516 0 0 0 4.74.627zM1.273 3.535l.994.633a.516.516 0 0 0 .555-.87l-.995-.633a.516.516 0 0 0-.554.87zM.878 8.043l1.15-.256a.516.516 0 1 0-.223-1.008l-1.15.256a.516.516 0 0 0 .111 1.02.535.535 0 0 0 .112-.012zm10.238-2.28a.535.535 0 0 0 .112-.012l1.15-.256a.516.516 0 1 0-.224-1.008l-1.15.256a.516.516 0 0 0 .112 1.02zM8.772 2.728a.516.516 0 0 0 .712-.158l.633-.994a.516.516 0 0 0-.87-.554l-.633.994a.516.516 0 0 0 .158.712zM3.07 7.032a3.506 3.506 0 0 0 .33.87 3.129 3.129 0 0 0 .909-.486 2.453 2.453 0 0 1-.233-.608 2.504 2.504 0 0 1 1.9-2.988 2.5 2.5 0 0 1 2.988 1.9c.003.013.002.026.005.038a5.42 5.42 0 0 1 1.063.25 3.509 3.509 0 0 0-.061-.512 3.535 3.535 0 1 0-6.902 1.536z"/>
-                        <path d="M12.715 8.48a3.236 3.236 0 0 0-.41.04 4.824 4.824 0 0 0-8.086 0 3.234 3.234 0 0 0-.409-.04 3.285 3.285 0 1 0 1.283 6.31 4.756 4.756 0 0 0 6.339 0 3.286 3.286 0 1 0 1.283-6.31zm0 5.539a2.238 2.238 0 0 1-.88-.179 1.032 1.032 0 0 0-1.083.173 3.724 3.724 0 0 1-4.98 0 1.032 1.032 0 0 0-1.082-.173 2.254 2.254 0 1 1-.88-4.329 1.265 1.265 0 0 1 .175.02l.105.014a1.031 1.031 0 0 0 .992-.459 3.792 3.792 0 0 1 6.36 0 1.031 1.031 0 0 0 .992.459l.105-.014a1.266 1.266 0 0 1 .176-.02 2.254 2.254 0 1 1 0 4.508z"/>
-                    </svg>
-                </li>`;
+                dayIconsInner += `<li><img src=../../assets/weather/${item.dayIconUrl}></li>`;
                 // 五：夜晚天气图标:
                 nightIconsInner += `<li>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="qi-101" viewBox="0 0 16 16">
